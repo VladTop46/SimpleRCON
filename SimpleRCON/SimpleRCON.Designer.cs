2736a49655a272eb8832b00fc86ae4de0561d70b
VladTop46 <mineserver123ab@gmail.com>
2021-12-30 19:02:22 +0900

    Добавьте файлы проекта.

---
+++ SimpleRCON/SimpleRCON.Designer.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.Http;
+using MinecraftServerRCON;
+
+namespace SimpleRCON
+{
+    partial class SimpleRCON
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ip_input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.port_input = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pass_input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 415);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(695, 23);
+            this.input.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(776, 397);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 469);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Адрес сервера:";
+            // 
+            // ip_input
+            // 
+            this.ip_input.Location = new System.Drawing.Point(108, 466);
+            this.ip_input.Name = "ip_input";
+            this.ip_input.Size = new System.Drawing.Size(148, 23);
+            this.ip_input.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(262, 469);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "RCON Порт:";
+            // 
+            // port_input
+            // 
+            this.port_input.Location = new System.Drawing.Point(342, 466);
+            this.port_input.Name = "port_input";
+            this.port_input.Size = new System.Drawing.Size(148, 23);
+            this.port_input.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(496, 469);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Пароль RCON:";
+            // 
+            // pass_input
+            // 
+            this.pass_input.Location = new System.Drawing.Point(590, 463);
+            this.pass_input.Name = "pass_input";
+            this.pass_input.Size = new System.Drawing.Size(148, 23);
+            this.pass_input.TabIndex = 8;
+            // 
+            // ConfigurableRCON
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 498);
+            this.Controls.Add(this.pass_input);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.port_input);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ip_input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ConfigurableRCON";
+            this.Text = "Configurable RCON Panel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox input;
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private TextBox ip_input;
+        private Label label2;
+        private TextBox port_input;
+        private Label label3;
+        private TextBox pass_input;
+    }
+}
---
+++ SimpleRCON/SimpleRCON.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.Http;
+using MinecraftServerRCON;
+
+namespace SimpleRCON
+{
+    public partial class SimpleRCON : Form
+    {
+        public SimpleRCON()
+        {
+            InitializeComponent();
+        }
+
+
+        public string port = String.Empty;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var rcon = RCONClient.INSTANCE)
+            {
+                if ((ip_input.Text != "") && (port_input.Text != "") && (pass_input.Text != ""))
+                {
+                    rcon.setupStream(ip_input.Text, port = port_input.Text, password: pass_input.Text);
+                    string answer = rcon.sendMessage(RCONMessageType.Command, input.Text);
+                    Console.WriteLine(answer.RemoveColorCodes());
+                    richTextBox1.Text = richTextBox1.Text + answer.RemoveColorCodes();
+                }
+                else
+                {
+                    MessageBox.Show("Не заполнены поля!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+    }
+}
